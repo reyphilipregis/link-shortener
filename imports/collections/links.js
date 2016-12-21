3f67d95d06ec8ba01aab617af7fdafd063e03fdc
Rey Philip Regis <reyphilipregis@gmail.com>
2016-12-21 10:23:19 +0800

    Chore: Add URL validation in meteor method links.insert

--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,9 +1,18 @@
 import { Mongo } from 'meteor/mongo';
+import validUrl from 'valid-url';
+import {check, Match } from 'meteor/check';
 
 // run codes in a secure fashion
 Meteor.methods( {
 	'links.insert' : (url) => {
-		console.log('attempting to save ' + url)
+		
+		// validating the url
+		check(url, 
+			  Match.Where( ( url ) => { 
+			  		return validUrl.isUri(url) 
+			  	}
+			  )
+		);
 	}
 } );
 
