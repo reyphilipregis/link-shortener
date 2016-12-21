17c7eed3bd9a0070f3269f4fbabbf7a1771fbcf5
Rey Philip Regis <reyphilipregis@gmail.com>
2016-12-21 16:38:37 +0800

    Chore: Update click counter in a link

--- server/main.js
+++ server/main.js
@@ -10,14 +10,24 @@ Meteor.startup(() => {
 		return Links.find({});
 	} );
 
-});
+} );
 
+// Executed whenever a user visits a route like localhost:3000/abcd
+function onRoute(req, res, next) {
+	const link 	   = Links.findOne( { token : req.params.token } );
+
+	if (link) {
+		Links.update( link,  { $inc : { clicks : 1 } } );
+
+		res.writeHead( 307, { 'Location' : link.url } );
+		res.end();
+	} else {
+		next();
+	}
+}
 
 const middleware = ConnectRoute( function (router) {
-	router.get('/:token', (req, res) => {
-		console.log(req.params.token);
-		res.end('====');
-	} );
+	router.get('/:token', onRoute);
 } );
 
 WebApp.connectHandlers.use( middleware );
